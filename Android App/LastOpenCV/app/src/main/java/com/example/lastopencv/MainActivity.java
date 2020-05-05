f7d03b4e4b487f72e72cfbaa6c90926c8e9dbcb1
Mohamed Ashraf <mashraf.7@hotmail.com>
2020-05-06 01:21:54 +0200

    Android app implementation

---
+++ Android App/LastOpenCV/app/src/main/java/com/example/lastopencv/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.lastopencv;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import org.opencv.android.OpenCVLoader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private VideoView videoView;
+    private static final String VIDEO_DIRECTORY = "/demonuts";
+    private int GALLERY = 1, CAMERA = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        videoView = (VideoView) findViewById(R.id.vv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
+
+    }
+
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Select video from gallery",
+                "Record video from camera" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                chooseVideoFromGallary();
+                                break;
+                            case 1:
+                                takeVideoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void chooseVideoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takeVideoFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d("result",""+resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            Log.d("what","cancle");
+            return;
+        }
+        if (requestCode == GALLERY) {
+            Log.d("what","gale");
+            if (data != null) {
+                Uri contentURI = data.getData();
+
+                String selectedVideoPath = getPath(contentURI);
+                Log.d("path",selectedVideoPath);
+                saveVideoToInternalStorage(selectedVideoPath);
+                videoView.setVideoURI(contentURI);
+                videoView.requestFocus();
+                videoView.start();
+
+            }
+
+        } else if (requestCode == CAMERA) {
+            Uri contentURI = data.getData();
+            String recordedVideoPath = getPath(contentURI);
+            Log.d("frrr",recordedVideoPath);
+            saveVideoToInternalStorage(recordedVideoPath);
+            videoView.setVideoURI(contentURI);
+            videoView.requestFocus();
+            videoView.start();
+        }
+    }
+
+    private void saveVideoToInternalStorage (String filePath) {
+
+        File newfile;
+
+        try {
+
+            File currentFile = new File(filePath);
+            File wallpaperDirectory = new File(Environment.getExternalStorageDirectory() + VIDEO_DIRECTORY);
+            newfile = new File(wallpaperDirectory, Calendar.getInstance().getTimeInMillis() + ".mp4");
+
+            if (!wallpaperDirectory.exists()) {
+                wallpaperDirectory.mkdirs();
+            }
+
+            if(currentFile.exists()){
+
+                InputStream in = new FileInputStream(currentFile);
+                OutputStream out = new FileOutputStream(newfile);
+
+                // Copy the bits from instream to outstream
+                byte[] buf = new byte[1024];
+                int len;
+
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+                Log.v("vii", "Video file saved successfully.");
+            }else{
+                Log.v("vii", "Video saving failed. Source file missing.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Video.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+}
