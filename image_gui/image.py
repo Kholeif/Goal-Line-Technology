1b21162e324ee07183cfcb68f11b04aab535303f
muhamedswelam <muhamed.swelam@gmail.com>
2020-05-06 17:24:20 +0200

    adding executable file

--- image_gui/image.py
+++ image_gui/image.py
@@ -54,6 +54,7 @@ class Ui_MainWindow(object):
 
 
 class Widget(QtWidgets.QWidget, Ui_MainWindow):
+
     def __init__(self, MainWindow):
         QtWidgets.QWidget.__init__(self)
         self.setupUi(MainWindow)
@@ -66,53 +67,40 @@ class Widget(QtWidgets.QWidget, Ui_MainWindow):
         greenUpper =(46, 255, 255)
         pts = deque(maxlen=20)
         video = cv2.VideoCapture(self.fileName)
-
         fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-        out = cv2.VideoWriter('outputt.avi',fourcc, 25.0,(1000,562),True)
-
         xmin = 1000
         xmin2= 1000
         flag = 1
         flag3 = 1
         final=None
-        ##xxx="No Goal"
+        xxx="No Goal"
         while(video.isOpened()):
             flag2= 0
             _, frame = video.read()
             if frame is None:
                 break
-        #     frame = imutils.rotate(frame, 180)
             frame = imutils.resize(frame, width=1000)
             (h, w) = frame.shape[:2]
-            
-            
-            
-            
-            #LINE detection  # detect xmin2
-
             if flag==1:
                 frame2=frame.copy()
                 hsv = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
-                lower_white = np.array([86  , 0 ,129])
-                upper_white = np.array([195 , 18, 255])
+                lower_white = np.array([0  , 0 ,171])
+                upper_white = np.array([255 , 56, 255])
 
                 mask = cv2.inRange(hsv, lower_white, upper_white)
 
                 kernel = np.ones((4,4), np.uint8)
                 mask = cv2.erode(mask, kernel, iterations=1)
                 mask = cv2.dilate(mask, kernel, iterations=1)
-                #cv2.imshow("mask",mask)
 
                 contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
                 contours = sorted(contours, key=cv2.contourArea, reverse=True)
                 if len(contours) > 0:
                     cv2.drawContours(frame2, [contours[0]], -1, (0, 255, 0), 5)
                     for i in range (0,len(contours[0])):
-                        #print(contours[0][i][0][0])
                         if contours[0][i][0][0] < xmin2:
                             xmin2=contours[0][i][0][0]
                     cv2.line(frame2, (xmin2,0),(xmin2,h),(0,0,225),5)
-                #cv2.imshow("frame2",frame2)
                 flag=0
                 
             
@@ -125,7 +113,6 @@ class Widget(QtWidgets.QWidget, Ui_MainWindow):
             mask = cv2.erode(mask, kernel, iterations=2)
             res=cv2.bitwise_and(frame,frame,mask=mask)
             cnts,_=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-            #cv2.imshow("mask",mask)
             
             center = None
             if len(cnts) > 0:
@@ -134,11 +121,9 @@ class Widget(QtWidgets.QWidget, Ui_MainWindow):
                 
                 if x<xmin and x>50:
                     xmin=x
-                    #final=frame.copy()
                     flag2=1
                 
                 if radius > 70: 
-                ##if True :
                     cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
                     cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
                     if x+radius < xmin2:
@@ -152,14 +137,9 @@ class Widget(QtWidgets.QWidget, Ui_MainWindow):
                 cv2.putText(frame, "Goal", (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 6)
                 
             cv2.line(frame, (xmin2,0),(xmin2,h),(0,0,225),5)
-        #     cv2.line(frame,point1,point2,(255,0,0),2)
             if flag2==1:
                 final=frame.copy()
             cv2.imshow("Frame", frame)
-            #(h, w) = frame.shape[:2]
-            #print(h)
-            #print(w)
-            out.write(frame)
             key = cv2.waitKey(1) & 0xFF
             
             if key == 27:
@@ -171,7 +151,6 @@ class Widget(QtWidgets.QWidget, Ui_MainWindow):
             cv2.imwrite("final.png",final)
 
         video.release()
-        out.release()
     def openFile(self):   
         self.fileName, _ = QFileDialog.getOpenFileName(self, "upload vedio",".", "Video Files (*.mp4 *.flv *.ts *.mts *.avi *.MOV)")
 
